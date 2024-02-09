c886054359c5949e023a3afc3d2f2eeab4ca9cf7
Sidhant <alairoet@gmail.com>
2024-02-10 03:42:11 +0530

    Refactor login function in auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,21 +4,38 @@ const axios = require("axios");
 
 module.exports = {
   login: async (req, res) => {
-   try {
-     const { token } = req.body;
-     const userInfo = await axios.get(
-       'https://www.googleapis.com/oauth2/v3/userinfo',
-       { headers: { Authorization: `Bearer ${token}` } },
-     );
-     const { sub:providerId, name, email, picture:avatar } = userInfo.data;
-     const user = await userService.findUserByProviderId({ providerId, name, email, avatar });
-     const accessToken = jwt.sign({ id: user._id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1y' });
-     console.log(`.${req.get("host")}`);
-     res.cookie('accessToken', accessToken, { httpOnly: true, secure: process.env.NODE_ENV === 'production', domain: process.env.NODE_ENV === 'production' ? process.env.DOMAIN : 'localhost'});
-     res.json({ user, accessToken });
-   } catch (error) {
+    try {
+      const { token } = req.body;
+      const userInfo = await axios.get(
+        "https://www.googleapis.com/oauth2/v3/userinfo",
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      const { sub: providerId, name, email, picture: avatar } = userInfo.data;
+      const user = await userService.findUserByProviderId({
+        providerId,
+        name,
+        email,
+        avatar,
+      });
+      const accessToken = jwt.sign(
+        { id: user._id },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1y" }
+      );
+      console.log(`.${req.get("host")}`);
+      res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        sameSite: "none",
+        secure: process.env.NODE_ENV === "production",
+        domain:
+          process.env.NODE_ENV === "production"
+            ? process.env.DOMAIN
+            : "localhost",
+      });
+      res.json({ user, accessToken });
+    } catch (error) {
       res.status(500).json({ message: error.message });
-   }
+    }
   },
   logout: async (_, res) => {
     try {
