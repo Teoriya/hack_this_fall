84f3f2a9d62247206c1edacb0e1e06d585cc87ad
Sidhant <alairoet@gmail.com>
2024-02-10 19:33:24 +0530

    Add @codemirror/state dependency and implement socket.io functionality

--- socket/index.js
+++ socket/index.js
@@ -1,4 +1,7 @@
 const origin = process.env.ORIGIN?.split(',') || "http://localhost:3000";
+const {ChangeSet, Text} = require("@codemirror/state")
+
+rooms = {};
 
 module.exports = function (server) {
     const io = require('socket.io')(server, {
@@ -10,7 +13,36 @@ module.exports = function (server) {
     })
 
     io.on('connection', (socket) => {
-        console.log('a user connected');
+        socket.on("fetch",({roomId})=>{
+            console.log(roomId)
+            if(!rooms[roomId]){
+                rooms[roomId]={
+                    doc:(Text.of(["Jai Shree Ram"])),
+                    updates:[],
+                };
+            }
+            socket.emit("pull",{version:rooms[roomId].updates.length,code:rooms[roomId].doc.toString()})
+            console.log(rooms)
+
+        })
+    
+        socket.on("push",({version,roomId,newUpdates})=>{
+            console.log("push",version,roomId,newUpdates)
+            if(!rooms[roomId])return console.warn("Invalid Websocket Request : Invalid Roomid");
+            if (rooms[roomId].updates.length != version) return;
+            for (let update of newUpdates){
+                let changes = ChangeSet.fromJSON(update.changes)
+                rooms[roomId].updates.push({changes,clientID:update.clientID})
+                rooms[roomId].doc = changes.apply(rooms[roomId].doc)
+            }
+            io.to(roomId).emit("sync",{newVersion:rooms[roomId].updates.length, updates:rooms[roomId].updates})
+    
+        })
+        socket.on('join', function ({roomId}) {
+            console.log("join",roomId)
+            socket.join(roomId);
+        });
+        
         socket.on('disconnect', () => {
             console.log('user disconnected');
         });
