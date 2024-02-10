2d441854686e5a333916e0f4e6162ab7400d412b
Sidhant <alairoet@gmail.com>
2024-02-11 05:20:54 +0530

    Add route for getting user data

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,6 +4,6 @@ const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/login', AuthController.login);
 router.post('/logout', authMiddleware ,AuthController.logout);
-
+router.get('/data', authMiddleware, AuthController.getUserData);
 
 module.exports = router;
